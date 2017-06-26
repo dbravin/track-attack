ae5e84cbcd8ca024b8fe1c83a88c941ca2ca77b4
Davide Bravin <bravin.davide@gmail.com>
2017-06-26 23:06:37 +0200

    enh: creating path and scaling to svg area

--- track.js
+++ track.js
@@ -1,12 +1,32 @@
 function track(){
 
 	function main(){
-		var data = d3.csv('adria.csv', draw)
+		var data = d3.csv('adria_cleaned.csv', draw)
 	}
 	function draw(data){
-		data = data.slice(23)
-		console.log(data[0])
-		d3.select('svg#track')
+		var svg = d3.select('svg#track')
+		var x = d3.extent(data, function(e){
+			return e.LAT
+		})
+		var y = d3.extent(data, function(e){
+			return e.LON
+		})
+		var scale_x = d3.scaleLinear()
+		.domain(x)
+		.range([0, 700])
+		var scale_y = d3.scaleLinear()
+		.domain(y)
+		.range([0, 400])
+		var line = d3.line()
+		.x(function (e){return scale_x(e.LAT)})
+		.y(function(e){return scale_y(e.LON)})
+		var g = svg.append('g')
+			.attr('transform', 'translate(0,0) scale(1)')
+			.append('path')
+			.attr('d', line(data))
+			.style('stroke', '#000')
+			.style('fill', 'none')
+
 	}
 	return {
 		main: main
